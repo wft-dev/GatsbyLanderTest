fa535cfd8744fc747051358a15d00de158b991e1
wft-dev <dev@myweb-development.com>
2020-01-20 15:58:47 +0530

    Added WP config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,38 @@ module.exports = {
 	      region: "ap-south-1"
 	    },
 	  },
+    ‘gatsby-plugin-react-helmet’,
+    ‘gatsby-transformer-remark’,
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `wordpress`,
+        fieldName: `wordpress`,
+        url: `https://wordpress-107195-1100370.cloudwaysapps.com/graphql`,
+      }
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `wordpress-107195-1100370.cloudwaysapps.com/`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        acfOptionPageIds: [“acf-options-header”, “theme-footer-settings”],
+        useACF: true
+      }
+    },
+    ‘gatsby-plugin-sharp’,
+    ‘gatsby-transformer-sharp’,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Oswald:400,500,600,700`,
+          `Open Sans:400,600,700,800`,
+          `PT Serif:400,700`
+        ],
+        display: ‘swap’
+      }
+    }
   ]
 };
